4b3c6c2433d5c0e2001af396ab8f3a5560346cfd
maria-natale <m.natale19@studenti.unisa.it>
2020-11-06 19:40:36 +0100

    finito quasi

--- Progetto_SAD/utenti_statisticaUnivariata.R
+++ Progetto_SAD/utenti_statisticaUnivariata.R
@@ -128,3 +128,24 @@ coefficienteVariazioneItalia<-sd(utenti_nazione)/abs(mean(utenti_nazione))
 coefficienteVariazioneItalia
 
 
+
+skw <-function (x){
+  n<-length (x)
+  m2 <-(n -1) *var (x)/n
+  m3 <- (sum ( (x- mean(x))^3) )/n
+  m3/(m2 ^1.5)
+}
+
+curt <-function (x){
+  n <-length (x)
+  m2 <-(n -1) *var (x)/n
+  m4 <- (sum ((x-mean(x))^4) )/n
+  m4/(m2 ^2) -3
+}
+
+skw(utenti_campania)
+skw(utenti_nazione)
+
+curt(utenti_campania)
+curt(utenti_nazione)
+
