98e5d4461c9855540525b9043bd5fc78a670ede7
gaelix98 <56078668+gaelix98@users.noreply.github.com>
2020-11-06 19:36:02 +0100

    update

--- Progetto_SAD/barplot vittime.r
+++ Progetto_SAD/barplot vittime.r
@@ -3,3 +3,5 @@ utenti=as.vector(t(utenti_campani))
 hist(utenti, breaks = 4, col = "red", main = "vittime della campania",xlim=c(219,738),ylab= "valori")
 v=c(2013,2014,2015,2016,2017,2018,2019,2020)
 barplot(utenti,width = 3,space = 2, names.arg=v, col = c("green","red","yellow","black","blue","brown","azure","gray"),xlab = "vittime per anni")
+
+barplot(mediavittimeitalia,width = 3,space = 2, names.arg=v, col = c("green","red","yellow","black","blue","brown","azure","gray"),xlab = "vittime per anni"))
